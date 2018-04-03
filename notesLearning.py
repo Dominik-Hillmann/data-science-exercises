08c6162bd11e9a4ba6342da3666401082da26c62
Dominik-Hillmann <dominik.hillmann@gmx.de>
2018-04-03 22:25:42 +0200

    Add files via upload

---
+++ notesLearning.py
@@ -0,0 +1,13 @@
+"""
+***** Unsupervised vs. Supervised Learning *****
+
+Unsupervised: you don't give it any answers it can learn from.
+> these algorithms try to find patterns in your data on their own
+> these pattern discoverd for example in "clustering" are called latent variablkes
+> examples: clustering movies via their properties and look by what mertrics they cluster together
+
+Supervised: we have a set of answers the model can learn from
+> Evaluation: two groups, a larger group of training data and test data that the model
+  hasn't seen yet to evaluate how good the model predicts unseen data
+> k
+"""
