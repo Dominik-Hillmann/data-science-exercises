86ee2c9e8741cd9bc7a34af5418f59c56d768dfe
Dominik-Hillmann <dominik.hillmann@gmx.de>
2018-07-12 21:26:03 +0200

    Add files via upload

---
+++ notes/notesMLandDataMining.py
@@ -0,0 +1,8 @@
+"""
+***** K-Nearest Neighbours *****
+
+> you already have a scatterplot along any dimensions with classified data points
+> a new data point is classified by identifying its k nearest neighbours and then classify the data points depending
+  on the class most of the k data points have
+
+"""
